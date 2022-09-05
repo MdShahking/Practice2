ae2c066dec428527f23b192cfc16f4e5e5281b18
Shahnawaz <shahnawazhussain738@gmail.com>
2022-09-05 18:32:41 +0530

    this is commited

---
+++ practice2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("enter your age");
+            //string agestr = Console.ReadLine();
+            //int age = Convert.ToInt32(agestr);
+            //switch (age)
+            //{
+            //    case 18:
+            //        Console.WriteLine("wait for a year");
+            //        break;
+            //        case 20:
+            //        Console.WriteLine("you are 20");
+            //        break ;
+            //        default:
+            //        Console.WriteLine("enjoy");
+            //        break;
+            int i = 0;
+            while (i <= 6)
+            {
+                Console.WriteLine(i);
+            i++;
+
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
